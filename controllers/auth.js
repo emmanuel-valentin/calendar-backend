8a7efc18c1ac2253b98d039e67ed974b3a5ac186
Emmanuel Valentin <emmanuel-valentin@outlook.com>
2024-01-14 01:55:15 -0600

    Fix typo

--- controllers/auth.js
+++ controllers/auth.js
@@ -74,7 +74,7 @@ const renewToken = async (req = request, res = response) => {
   const { uid, name } = req;
   const token = await generateJWT(uid, name);
 
-  res.json({ ok: true, ui, name, token });
+  res.json({ ok: true, uid, name, token });
 };
 
 module.exports = {
